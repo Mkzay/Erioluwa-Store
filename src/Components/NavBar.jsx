a6f5ccedaa00617471bef546fb1b7131c7b224f4
Mkzay <80535194+Mkzay@users.noreply.github.com>
2024-07-06 10:27:34 +0100

    font size

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
           menu
             ? "-translate-y-[250%] duration-1000 ease-out"
             : "translate-y-0 duration-1000 ease-in"
-        } w-full h-full flex flex-col items-center gap-2.5 fixed top-[150px] pt-[23px] text-db text-xl/[37.2px] font-bold backdrop-filter backdrop-blur-sm`}
+        } w-full h-full flex flex-col items-center gap-2.5 fixed top-[150px] pt-[23px] text-db text-3xl/[37.2px] font-bold backdrop-filter backdrop-blur-sm`}
       >
         <li>Home</li>
         <li>Cart</li>
